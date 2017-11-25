155a22c33797e34632ab1a1a1f551d926d2a9d4c
hosamk92 <hosamk92@gmail.com>
2017-11-25 19:48:55 +0200

    Create CF49-D2-C.cpp

---
+++ Codeforces/CF49-D2-C.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <fstream>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+using namespace std;
+
+double Fib(double x,double z);
+string BinDec(ull x);
+string StringInt(ll x);
+ull StringInt(string x);
+ull BinDec (string x);
+ull POWMOD (ull x,ull y,ull mod);
+ull POWE(long long , long long);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+ //   freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+
+    int x,i;
+    cin>>x;
+    if(x==1)cout<<1;
+    else{
+        cout<<x<<' ';
+        for(i=1;i<=x-1;i++)
+        {
+            cout<<i<<' ';
+        }
+      //  cout<<1;
+    }
+
+    return 0;
+}
