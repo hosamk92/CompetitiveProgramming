83648a0e41a2964dc341dfd2661565c300a9e950
hosamk92 <hosamk92@gmail.com>
2018-02-03 00:52:59 +0200

    Update SRM536-D2-1000.cpp

--- TopCoder/SRM536-D2-1000.cpp
+++ TopCoder/SRM536-D2-1000.cpp
@@ -23,33 +23,62 @@ using namespace std;
 
 class MergersDivTwo {
 public:
-    double x;
-    double rec(int i,vector<int>v,double k,double s,int r)
+    int x;
+    double rec(int i,vector<int>v,int k,long double s)
     {
-        double ans=-1e8;;
+        long double ans=-1e8;;
      //   cout<<s<<' '<<k<<'\n';
         if(v.size()==i&&k>=x)return s;
         if(v.size()==i)return -1e8;
-        if(k==x&&r)
+
+        if(k>=x&&k<x*2)
         {
-            ans=rec(i+1,v,2,(s+v[i])/2,r+1);
-            if(ans!=-1e8)return ans;
+            ans=max(rec(i+1,v,2,(s+v[i])/2),rec(i+1,v,k+1,((s*k)+v[i])/(k+1)));
         }
-        if(k>=x&&r==0)
+        else if(k>=x)
         {
-            ans=rec(i+1,v,2,(s+v[i])/2,r+1);
-            if(ans!=-1e8)return ans;
-            ans=rec(i+1,v,k+1,((s*k)+v[i])/(k+1),r);
-            if(ans!=-1e8)return ans;
+        	 ans=rec(i+1,v,2,(s+v[i])/2);
         }
         if(k<x)
-        ans=rec(i+1,v,k+1,((s*k)+v[i])/(k+1),r);
+        ans=rec(i+1,v,k+1,((s*k)+v[i])/(k+1));
 
         return ans;
     }
-	double findMaximum(vector <int> v, int k) {
+    double rec1(int i,vector<int>v,int k,long double s)
+    {
+        long double ans=-1e8;;
+     //   cout<<s<<' '<<k<<'\n';
+        if(v.size()==i&&k>=x)return s;
+        if(v.size()==i)return -1e8;
+
+        if(k>=x&&k<(int)x+(x/2))
+        {
+            ans=max(rec1(i+1,v,2,(s+v[i])/2),rec1(i+1,v,k+1,((s*k)+v[i])/(k+1)));
+        }
+        else if(k>=x)
+        {
+        	 ans=rec1(i+1,v,2,(s+v[i])/2);
+        }
+        if(k<x)
+        ans=rec1(i+1,v,k+1,((s*k)+v[i])/(k+1));
+
+        return ans;
+    }
+	long double findMaximum(vector <int> v, int k) {
 		sort (v.begin(),v.end());
 		x=k;
-		return rec(1,v,1,v[0],0);
+		if(k==2)
+        {
+            double ans=v[0];
+            for(int i=1;i<v.size();i++)
+            {
+                ans=(ans+v[i])/2;
+            }
+            return ans;
+        }
+		if(k<=4)return rec1(0,v,0,0);
+		cout<<v.size()<<endl;
+		return rec(0,v,0,0);
 	}
 };
+
