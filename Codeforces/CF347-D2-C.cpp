318ed18e0d54394eed873110f64237e41d60cb9f
hosamk92 <hosamk92@gmail.com>
2018-02-11 21:52:46 +0200

    Create CF347-D2-C.cpp

---
+++ Codeforces/CF347-D2-C.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include <cfloat>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+using namespace std;
+
+const double PI = acos(-1.0);
+
+const double EPS = 1e-9;
+
+typedef complex<float> point;
+
+
+#define angle(a)                (atan2((a).imag(), (a).real()))
+#define vec(a,b)                ((b)-(a))
+#define same(p1,p2)             (dp(vec(p1,p2),vec(p1,p2)) < EPS)
+#define dp(a,b)                 ( (conj(a)*(b)).real() )	// a*b cos(T), if zero -> prep
+#define cp(a,b)                 ( (conj(a)*(b)).imag() )	// a*b sin(T), if zero -> parllel
+#define length(a)               (hypot((a).imag(), (a).real()))
+#define normalize(a)            (a)/length(a)
+//#define polar(r,ang)            ((r)*exp(point(0,ang)))  ==> Already added in c++11
+#define rotateO(p,ang)          ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+ll fast_pow(ll a,ll b)
+{
+	if(b==0)
+		return 1ll;
+	ll ans=1;
+	if(b%2)
+		return ans=(fast_pow(a,b-1)*a);
+	ans=(fast_pow(a,b/2));
+	return ((ans*ans));
+}
+
+int main()
+{
+
+   // freopen("tshirts.in","r",stdin);
+    //freopen("game.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll x,i,j,s=0,a[200],y=0,c=0,z=1;
+    cin>>x;
+    for(i=0;i<x;i++){
+        cin>>a[i];
+        y=max(a[i],y);
+    }
+    sort(a,a+x);
+    for(i=0;i<x;i++)
+    {
+        if(i==0)z=a[i];
+        else
+            z=__gcd(a[i],z);
+    }
+
+        s=(y/z)-x;
+    if(s%2)cout<<"Alice";
+    else cout<<"Bob";
+
+    return 0;
+}
